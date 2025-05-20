8ee4ccdf3a43d1ced30f71b05dadfb2c7c300b37
Hemant Modi <hemantmodi101@gmail.com>
2025-05-20 13:14:16 +0530

    minor bug

--- src/app/tables/page.tsx
+++ src/app/tables/page.tsx
@@ -1,7 +1,5 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import TableOne from "@/components/Tables/TableOne";
 import TableThree from "@/components/Tables/TableThree";
-import TableTwo from "@/components/Tables/TableTwo";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
 const TablesPage = () => {
@@ -10,8 +8,6 @@ const TablesPage = () => {
       <Breadcrumb pageName="Tables" />
 
       <div className="flex flex-col gap-10">
-        <TableOne />
-        <TableTwo />
         <TableThree />
       </div>
     </DefaultLayout>
