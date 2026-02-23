af3123df21eaf1a6e2d86054ed21a7f471567293
Hemant Modi <hemantmodi101@gmail.com>
2026-02-24 00:16:28 +0530

    url change

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: "https://backend.aismartexhibits.com/",
+  baseURL: "https://server.aismartexhibits.com/",
 });
 
 export default customAxios;
